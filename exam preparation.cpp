4ff802c0b8ad3d810f07a5016ce1264679259aa8
Abhinav5678 <122282202+Abhinav5678@users.noreply.github.com>
2023-01-13 23:54:30 +0530

    Create exam preparation.cpp

---
+++ exam preparation.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{
+	int a,b,c;
+	printf("ENTER THE FIRST NUMBER HERE ");
+	scanf("%d",&a);
+    if (a&1==0)
+    printf("NOT");
+    else 
+    printf("set");
+    
+     
+
+
+
+}
